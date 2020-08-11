54284745b080de1520daabf993baf1c91cb56ebd
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 13:19:53 -0700

    react

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   renderSubmit(language) {
-    return value === "english" ? "Submit" : "Voorleggen";
+    return language === "english" ? "Submit" : "Voorleggen";
   }
 
   render() {
