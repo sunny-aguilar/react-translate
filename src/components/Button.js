36fd4dc40dc6f83d716a78e8e79dd2d6bc1d4a9f
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 15:05:08 -0700

    react

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react";                                  // load react library
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
 
