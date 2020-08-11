5eb898382c98e6f67109c41e12f5bb8fa06cb114
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 13:00:09 -0700

    react

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,6 @@ import ColorContext from "../contexts/ColorContext";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-  state = { language: "english" };
-
   onLanguageChange = (language) => {
     this.setState({ language: language });
   }
