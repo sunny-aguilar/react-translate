3436547e238e89a1a9fd22989746ab5ce33f3ea0
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 15:05:45 -0700

    react

--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -1,6 +1,6 @@
 import React from "react";      // load react library
 import Field from "./Field";    // load Field component
-import Button from "./Button";
+import Button from "./Button";  // load Button component
 
 const UserCreate = () => {
   return (
