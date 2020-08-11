fda5f05a354a0b99fd39c50b4b9da4d9933da621
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 14:56:49 -0700

    react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";          // load react library
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom";   // load react dom library
+import App from "./components/App"; // import App component
 
 ReactDOM.render(
   <App />,
