ef93a31ef5e9771f345890337cbb1720e8eed6d6
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 12:09:17 -0700

    react

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -3,7 +3,11 @@ import React from "react";
 class LangugageSelector extends React.Component {
   render() {
     return (
-
+      <div>
+        Select a language:
+        <i className="flag us" onClick={() => this.onLanguageChange("english")} />
+        <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+      </div>
     );
   }
 }
