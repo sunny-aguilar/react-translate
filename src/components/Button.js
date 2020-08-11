0a166935a2aaa817a5182b74f83f57cf06fb7972
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 12:03:36 -0700

    react

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class Button extends React.Component {
+  // how to wire LanguageContext (contextType is a special name that must be used)
+  // static contextType = LanguageContext;
+
+  render() {
+    // const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return (
+      <ColorContext.Consumer>
+        {(color) =>
+          <button className={ `ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(value) => value === "english" ? "Submit" : "Voorleggen"}
+            </LanguageContext.Consumer>
+          </button>
+        }
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+// only this is needed to create a context object (the data pipe)
+export default React.createContext("english");
