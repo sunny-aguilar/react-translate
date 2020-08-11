c571dbd8a64ec8baf03d5084d028970143a45ff7
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-11 12:52:02 -0700

    react

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -5,6 +5,11 @@ const Context = React.createContext("english");
 
 class LanguageStore extends React.Component {
   state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
   render() {
 
   }
